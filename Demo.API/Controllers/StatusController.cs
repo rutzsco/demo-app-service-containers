f3eddd7231f779b0f9bc21bf2ecb6be6e123bb60
Scott Rutz <50925884+rutzsco@users.noreply.github.com>
2022-01-04 12:21:32 -0600

    Update StatusController.cs

--- Demo.API/Controllers/StatusController.cs
+++ Demo.API/Controllers/StatusController.cs
@@ -30,10 +30,13 @@ namespace DemoAPI.Controllers
         [HttpGet("status/full")]
         public IEnumerable<KeyValuePair<string, string>> GetFull()
         {
-            var variables = new List<KeyValuePair<string, string>>();
-            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
-                variables.Add(new KeyValuePair<string, string>(de.Key.ToString(), de.Value.ToString()));
-            return variables;
+            var values = _configuration.AsEnumerable().Select(c => new KeyValuePair<string,string>(c.Key,c.Value));
+            return values;
+
+            //var variables = new List<KeyValuePair<string, string>>();
+            //foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+            //    variables.Add(new KeyValuePair<string, string>(de.Key.ToString(), de.Value.ToString()));
+            //return variables;
         }
     }
 }
